1757f6a193560ece04b84afab65b685240054491
Konstantin Shvyryaev <konstantin.shvyryaev@yandex.ru>
2021-02-17 01:35:36 +0300

    k8s deployment and cluster ip services added

--- posts/index.js
+++ posts/index.js
@@ -23,7 +23,7 @@ app.post('/posts', async (req, res) => {
     title
   };
 
-  await axios.post('http://localhost:4005/events', {
+  await axios.post('http://event-bus-clusterip-srv:4005/events', {
     type: 'PostCreated',
     data: {
       id,
@@ -35,7 +35,7 @@ app.post('/posts', async (req, res) => {
 });
 
 app.post('/events', (req, res) => {
-  console.log('Received Event', req.body.type);
+  console.log('Received Event:', req.body.type);
 
   res.send({});
 });
